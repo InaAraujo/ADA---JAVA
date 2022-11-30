455ee790fa99a852460420b7075fc4d5d47d9984
Ina Araujo <inaiaraaraujo25@hotmail.com>
2022-11-30 16:31:50 -0300

    Atualização dos exercicios

--- src/ExerciciosCondicionais3.java
+++ src/ExerciciosCondicionais3.java
@@ -2,24 +2,46 @@ import java.util.Scanner;
 public class ExerciciosCondicionais3 {
     public static void main(String[] args) {
 
-        Scanner scannerNum = new Scanner(System.in);
-        System.out.println("Digite os Graus que deseja converte");
-        double num = scannerNum.nextInt();
+        double num = digitarGraus();
+        String letra = digitarLetra();
 
-        Scanner ScannerLetra = new Scanner(System.in);
-        System.out.println("Digite C para converter para Celsius ou F para farenheit");
-        String letra = ScannerLetra.nextLine();
 
-        if (letra.equals ("C"))
-        {double celsius = num - 32 / 1.8;
+        if (letra.equals ("C")){
+           double celsius = calcularCelsius(num);
             System.out.println("O valor convertido é" + celsius);
+
         } else if (letra.equals("F")) {
-            double farenheit = num * 1.8 + 32;
+            double farenheit = calcularFarenheit(num);
             System.out.println("O valor convertido é " + farenheit);
+
         }else {
             System.out.println("Valor invalido");
 
         }
     }
+
+    static double digitarGraus(){
+        Scanner scannerNum = new Scanner(System.in);
+        System.out.println("Digite os Graus que deseja converte");
+        double num = scannerNum.nextInt();
+        return num;
+    }
+
+    static String digitarLetra(){
+        Scanner ScannerLetra = new Scanner(System.in);
+        System.out.println("Digite C para converter para Celsius ou F para farenheit");
+        String letra = ScannerLetra.nextLine();
+        return letra;
+    }
+
+    static double calcularCelsius(double num){
+       return num - 32 / 1.8;
+    }
+
+    static double calcularFarenheit(double num){
+        return num * 1.8 + 32;
+    }
+
+
 }
 
